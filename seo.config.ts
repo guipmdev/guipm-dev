2e3574ef4332fe94ab27f09b48445eb1793cd340
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-21 14:40:10 -0300

    feat(layout): add SEO configuration

---
+++ seo.config.ts
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next'
+
+const defineMetadata = <T extends Metadata>(metadata: T) => metadata
+
+export const seoConfig = defineMetadata({
+  title: {
+    template: '%s - guipm.dev',
+    default:
+      'guipm.dev - Desenvolvedor Full Stack | React.js | Node.js | TypeScript | Express.js | PostgreSQL',
+  },
+  description:
+    'Desenvolvedor Full Stack | React.js | Node.js | TypeScript | Express.js | PostgreSQL',
+  manifest: '/site.webmanifest',
+  icons: [
+    { rel: 'icon', url: '/favicon.ico' },
+    { rel: 'apple-touch-icon', url: '/apple-touch-icon.png' },
+    { rel: 'mask-icon', url: '/favicon.ico' },
+    { rel: 'image/x-icon', url: '/favicon.ico' },
+  ],
+})
