399926a9ea4ace1d3e36357cda120184d13d1b1e
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-08-21 16:03:45 -0300

    style: update card item description styling and behavior

--- src/components/CardItem/Description/index.tsx
+++ src/components/CardItem/Description/index.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from 'react'
 import { useIsMounted } from 'usehooks-ts'
 
-import { css, cx } from '@/styled-system/css'
+import { css } from '@/styled-system/css'
 
 import { ParagraphCardItemContainer } from './styles'
 
@@ -12,30 +12,65 @@ interface DescriptionCardItemProps {
 }
 
 export function DescriptionCardItem({ description }: DescriptionCardItemProps) {
-  const ref = useRef<HTMLDivElement | null>(null)
+  const divRef = useRef<HTMLDivElement | null>(null)
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+
   const [hasOverflow, setHasOverflow] = useState(false)
+  const [isExpanded, setIsExpanded] = useState(false)
 
   const isMounted = useIsMounted()
 
-  useEffect(() => {
-    if (ref?.current) {
-      ref.current.className = cx(
-        ref.current.className,
-        css({
-          overflow: 'hidden',
-          display: '-webkit-box',
-          WebkitLineClamp: 10,
-          lineClamp: 10,
-        }),
+  const lineHeightInRem = 1.25
+  const initialVisibleLinesNumber = 10
+
+  const defaultMaxHeight = `calc(${lineHeightInRem}rem * ${initialVisibleLinesNumber})`
+  const defaultMaskPosition = 'center 100%'
+  const defaultMarginTop = 'calc((2.625rem + 0.75rem) * -1)'
+
+  function handleToggleDescription() {
+    if (divRef?.current) {
+      const remSizeInPx = parseInt(
+        getComputedStyle(document.documentElement).fontSize,
       )
 
-      setHasOverflow(ref.current.scrollHeight > ref.current.clientHeight)
+      const divHeightInPx = divRef.current.scrollHeight
+      const lineHeightInPx = lineHeightInRem * remSizeInPx
+
+      const linesNumber = Math.ceil(divHeightInPx / lineHeightInPx)
+
+      divRef.current.style.maxHeight = isExpanded
+        ? defaultMaxHeight
+        : `calc(${lineHeightInRem}rem * ${linesNumber})`
+
+      divRef.current.style.maskPosition = isExpanded
+        ? defaultMaskPosition
+        : 'center 0%'
+
+      setIsExpanded(!isExpanded)
     }
-  }, [description, isMounted])
+
+    if (buttonRef?.current) {
+      buttonRef.current.style.marginTop = isExpanded ? defaultMarginTop : '0'
+    }
+  }
+
+  useEffect(() => {
+    if (divRef?.current) {
+      divRef.current.className = css({
+        maskImage: 'linear-gradient(to bottom, black 50%, transparent 100%)',
+        maskSize: 'auto 200%',
+      })
+
+      divRef.current.style.maxHeight = defaultMaxHeight
+      divRef.current.style.maskPosition = defaultMaskPosition
+
+      setHasOverflow(divRef.current.scrollHeight > divRef.current.clientHeight)
+    }
+  }, [defaultMaxHeight, isMounted])
 
   return (
     <ParagraphCardItemContainer>
-      <div ref={ref}>
+      <div ref={divRef}>
         {description.map((paragraph, index) =>
           paragraph.length ? (
             <p key={paragraph + index}>{paragraph}</p>
@@ -45,7 +80,18 @@ export function DescriptionCardItem({ description }: DescriptionCardItemProps) {
         )}
       </div>
 
-      {hasOverflow && <button>Expandir</button>}
+      {hasOverflow && (
+        <button
+          onClick={handleToggleDescription}
+          ref={buttonRef}
+          className={css({
+            margin: '0 auto',
+            marginTop: defaultMarginTop,
+          })}
+        >
+          {isExpanded ? 'Ocultar' : 'Expandir'}
+        </button>
+      )}
     </ParagraphCardItemContainer>
   )
 }
--- src/components/CardItem/Description/styles.ts
+++ src/components/CardItem/Description/styles.ts
@@ -2,8 +2,20 @@ import { styled } from '@/styled-system/jsx'
 
 export const ParagraphCardItemContainer = styled('div', {
   base: {
+    position: 'relative',
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    gap: '0.75rem',
+
     '& > div': {
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+
       marginBottom: '0.5rem',
+
+      transition: 'max-height 0.5s, mask-position 0.5s',
 
       '& p': {
         textStyle: 'sm',
@@ -11,6 +23,31 @@ export const ParagraphCardItemContainer = styled('div', {
         '& + p': {
           marginTop: '0.25rem',
         },
+      },
+    },
+
+    '& button': {
+      position: 'relative',
+
+      padding: '0.5rem',
+      border: '1px solid white',
+      borderColor: 'title',
+      borderRadius: '6px',
+
+      textStyle: 'md',
+      textTransform: 'uppercase',
+      color: 'title',
+
+      cursor: 'pointer',
+
+      transition: 'color 0.2s, background-color 0.2s, margin 0.5s',
+
+      '&:hover': {
+        backgroundColor: 'title',
+
+        borderColor: 'title',
+
+        color: 'background',
       },
     },
   },
